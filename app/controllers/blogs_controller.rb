63c41082271c39def483ae207ce3fbd51f09ad45
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-10-28 19:12:22 +0900

    add:favorite

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,6 +6,7 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
 
   def new
@@ -17,6 +18,7 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
       if @blog.save
         redirect_to blog_path(@blog.id)
       else
